523e4a66606e4f3f218bddded9823658260eb7cc
Nathan Rodrigues Costa <nathan.costa@grupopll.com.br>
2022-03-15 02:38:51 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webApiFilmes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Models.Filme> filmes = new List<Models.Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Models.Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
